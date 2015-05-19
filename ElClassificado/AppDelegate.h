f888290071370a07a025f34dbd35f0ce76612365
purva1910 <purva.bhureja@russmediatech.in>
2015-05-19 10:04:27 +0530

    Initial Commit

---
+++ ElClassificado/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ElClassificado
+//
+//  Created by Abhishek on 19/05/15.
+//  Copyright (c) 2015 Abhishek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ElClassificado/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ElClassificado
+//
+//  Created by Abhishek on 19/05/15.
+//  Copyright (c) 2015 Abhishek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
